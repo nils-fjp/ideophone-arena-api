5a9113a92f44a7539cb9b9644cf3b356619e5e8a
nils-fjp <nils.fj.paulsson@proton.me>
2026-05-20 11:34:48 +0200

    initial commit

---
+++ src/main/java/io/github/nilsfjp/ideophonearena/IdeophoneArenaApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.nilsfjp.ideophonearena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeophoneArenaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdeophoneArenaApiApplication.class, args);
+    }
+
+}
